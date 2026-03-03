416d6914d2f1ee8c107ab687c5374fffbd103765
kevh2003 <kevh.dev21@gmail.com>
2026-03-03 04:05:24 +0000

    Added working Torches : press T to toggle, added more lighting

---
+++ Assets/Game/World/TorchHeld.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorchHeld : MonoBehaviour
+{
+    [SerializeField] private Light torchLight;
+    [SerializeField] private bool startOn = true;
+
+    private bool _isOn;
+
+    private void Awake()
+    {
+        if (torchLight == null)
+            torchLight = GetComponentInChildren<Light>(true);
+
+        Set(startOn);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.tKey.wasPressedThisFrame)
+            Toggle();
+    }
+
+    public void Toggle() => Set(!_isOn);
+
+    private void Set(bool on)
+    {
+        _isOn = on;
+        if (torchLight != null)
+            torchLight.enabled = on;
+    }
+}
